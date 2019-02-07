f042cf3776918f5b8b828e967aa6dc591a5aa234
Pranay Agarwal <pranayag15@gmail.com>
2019-02-07 08:55:10 +0000

    fuck registrations

--- routes/auth.js
+++ routes/auth.js
@@ -23,68 +23,68 @@ router.get("/register", loggedIn,(req, res) => {
     res.render("signup-in", data);
 });
 
-router.post("/register",(req, res) => {
-    req.checkBody('name1', 'You must have a name').notEmpty();
-    req.checkBody('name2', 'You must have a name').notEmpty();
-    req.checkBody('email', 'Email is empty').isEmail().notEmpty();
-    req.checkBody('college', 'You must be in college').notEmpty();
-    req.checkBody('gender', 'Specify Gender').notEmpty();
-    req.checkBody('city', 'Must not be a vagabond').notEmpty();
-    req.checkBody('phone', 'Enter valid phone number').notEmpty();
-    req.checkBody('password', 'Enter valid Password').notEmpty();
-    req.checkBody('confirm-password', 'password confirmation fails').equals(req.body.password).notEmpty();
+// router.post("/register",(req, res) => {
+//     req.checkBody('name1', 'You must have a name').notEmpty();
+//     req.checkBody('name2', 'You must have a name').notEmpty();
+//     req.checkBody('email', 'Email is empty').isEmail().notEmpty();
+//     req.checkBody('college', 'You must be in college').notEmpty();
+//     req.checkBody('gender', 'Specify Gender').notEmpty();
+//     req.checkBody('city', 'Must not be a vagabond').notEmpty();
+//     req.checkBody('phone', 'Enter valid phone number').notEmpty();
+//     req.checkBody('password', 'Enter valid Password').notEmpty();
+//     req.checkBody('confirm-password', 'password confirmation fails').equals(req.body.password).notEmpty();
 
-    var errors = req.validationErrors();
-    if(errors){
-        req.flash('registrationError', errors);
-        console.log(req.flash('registrationError'));
-        res.render("signup-in",{
-            name1: req.body.name1,
-            name2: req.body.name2,
-            email: req.body.email,
-            gender: req.body.gender,
-            college: req.body.college,
-            city: req.body.city,
-            phone: req.body.phone,
-            errorMessages: errors
-        })
-        // res.render("register");
-    } else {
-        var fullname = req.body.name1+" "+req.body.name2;
-        // console.log("full name: ", fullname);
-        var newUser = new models.User({
-            name: fullname,
-            username: req.body.email,
-            college: req.body.college,
-            city: req.body.city,
-            phone: req.body.phone,
-            gender: req.body.gender,
-            paytmpayment: [],
-            password: req.body.password
-        });
-        models.User.register(newUser, req.body.password, function(err, user){
-            if(err){
-                // console.log(err.message);
-                req.flash('existingUsername', "Entered email is already registered");
-                res.render("signup-in",{
-                    name1: req.body.name1,
-                    name2: req.body.name2,
-                    email: req.body.email,
-                    college: req.body.college,
-                    gender: req.body.gender,
-                    gender: req.body.gender,
-                    city: req.body.city,
-                    phone: req.body.phone
-                });
-            } else {
-                passport.authenticate("local")(req, res, function(){
-                    // console.log(req.session);
-                    res.redirect("/events");
-                })
-            }
-        })
-    }
-});
+//     var errors = req.validationErrors();
+//     if(errors){
+//         req.flash('registrationError', errors);
+//         console.log(req.flash('registrationError'));
+//         res.render("signup-in",{
+//             name1: req.body.name1,
+//             name2: req.body.name2,
+//             email: req.body.email,
+//             gender: req.body.gender,
+//             college: req.body.college,
+//             city: req.body.city,
+//             phone: req.body.phone,
+//             errorMessages: errors
+//         })
+//         // res.render("register");
+//     } else {
+//         var fullname = req.body.name1+" "+req.body.name2;
+//         // console.log("full name: ", fullname);
+//         var newUser = new models.User({
+//             name: fullname,
+//             username: req.body.email,
+//             college: req.body.college,
+//             city: req.body.city,
+//             phone: req.body.phone,
+//             gender: req.body.gender,
+//             paytmpayment: [],
+//             password: req.body.password
+//         });
+//         models.User.register(newUser, req.body.password, function(err, user){
+//             if(err){
+//                 // console.log(err.message);
+//                 req.flash('existingUsername', "Entered email is already registered");
+//                 res.render("signup-in",{
+//                     name1: req.body.name1,
+//                     name2: req.body.name2,
+//                     email: req.body.email,
+//                     college: req.body.college,
+//                     gender: req.body.gender,
+//                     gender: req.body.gender,
+//                     city: req.body.city,
+//                     phone: req.body.phone
+//                 });
+//             } else {
+//                 passport.authenticate("local")(req, res, function(){
+//                     // console.log(req.session);
+//                     res.redirect("/events");
+//                 })
+//             }
+//         })
+//     }
+// });
 
 router.get("/dashboard", isLoggedin,(req, res)=>{
     if(req.user){
